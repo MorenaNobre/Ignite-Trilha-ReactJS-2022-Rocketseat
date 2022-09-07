bd20d881952a4eb5e3e6b2afd523ff4274cdd470
MorenaNobre <morenagnobre@gmail.com>
2022-09-07 17:10:49 -0300

    update README & Header component

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,12 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
+
+import "./styles.css";
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Morena Nobre"
         content="Um post muito da hora que fala sobre coisas muito legais"

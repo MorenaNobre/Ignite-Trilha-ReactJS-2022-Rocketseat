860f0cfc4394ee58fc7fa0bba9c360cfcc6c6a57
MorenaNobre <morenagnobre@gmail.com>
2022-09-12 17:19:39 -0300

    Added logo to Header component

--- 01-fundamentos-reactjs/src/components/Header.jsx
+++ 01-fundamentos-reactjs/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import styles from "./Header.module.css";
 
+import igniteLogo from "../assets/logo-ignite.svg";
+
 export function Header() {
   return (
     <header className={styles.header}>
-      <h1>Ignite Feed</h1>
+      <img src={igniteLogo} alt="Logotipo do Ignite" />
     </header>
   )
 }

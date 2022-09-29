ca87cd1725b93febc22f7c4be1f7a0a7b9147c88
MorenaNobre <morenagnobre@gmail.com>
2022-09-29 13:07:02 -0300

    Type spread

--- 01-fundamentos-reactjs-ts/src/components/Avatar.tsx
+++ 01-fundamentos-reactjs-ts/src/components/Avatar.tsx
@@ -1,18 +1,16 @@
+import { ImgHTMLAttributes } from "react";
 import styles from "./Avatar.module.css";
 
-interface AvatarProps {
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement>{
   hasBorder?: boolean;
-  src: string;
-  alt?: string;
 }
 
-export function Avatar({hasBorder = true, src, alt}: AvatarProps) {
+export function Avatar({hasBorder = true, ...props}: AvatarProps) {
   return (
     <div>
       <img
         className={hasBorder ? styles.avatarWithBorder : styles.avatar}
-        src={src}
-        alt={alt}
+        {...props}
       />
     </div>
   );

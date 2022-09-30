32850c37e8c99df9e7f66b2d90c87d6e2afc53cc
MorenaNobre <morenagnobre@gmail.com>
2022-09-30 10:48:54 -0300

    Tasks component

--- toDoList-Challenge/src/App.tsx
+++ toDoList-Challenge/src/App.tsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header"
+import { Tasks } from "./components/Tasks"
 
 function App() {
   return (
     <>
       <Header />
+      <Tasks />
     </>
   )
 }
---
+++ toDoList-Challenge/src/components/Tasks/index.tsx
@@ -0,0 +1,18 @@
+import styles from "./Tasks.module.css";
+
+export function Tasks() {
+  return (
+    <section className={styles.tasks}>
+      <header className={styles.header}>
+        <div>
+          <p>Tarefas criados</p>
+          <span>10</span>
+        </div>
+        <div>
+          <p className={styles.textPurple}>Tarefas concluídas</p>
+          <span>2 de 10</span>
+        </div>
+      </header>
+    </section>
+  )
+}

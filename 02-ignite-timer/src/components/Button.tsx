cd15674e4d1394cc6955739586ec2dada2cf24b1
MorenaNobre <morenagnobre@gmail.com>
2022-10-03 12:46:55 -0300

    Setting ESlint

--- 02-ignite-timer/src/components/Button.tsx
+++ 02-ignite-timer/src/components/Button.tsx
@@ -4,10 +4,10 @@ interface ButtonProps {
   variant?: ButtonVariant;
 }
 
-export function Button({variant = "primary"}: ButtonProps) {
+export function Button({ variant = "primary" }: ButtonProps) {
   return (
     <>
       <ButtonContainer variant={variant}>Enviar</ButtonContainer>
     </>
-  )
+  );
 }

02a0173383b27e7da3be50e73fe4420b95d5339e
MorenaNobre <morenagnobre@gmail.com>
2022-10-04 11:30:21 -0300

    Setting Routes with React Router DOM

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
 
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
@@ -7,13 +8,10 @@ import { defaultTheme } from "./styles/themes/default";
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }

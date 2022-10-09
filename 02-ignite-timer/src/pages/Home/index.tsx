1a68938741ad9133a1b1a0c2f4f0d1ada740bdbe
MorenaNobre <morenagnobre@gmail.com>
2022-10-09 16:46:10 -0300

    Form reset

--- 02-ignite-timer/src/pages/Home/index.tsx
+++ 02-ignite-timer/src/pages/Home/index.tsx
@@ -31,7 +31,7 @@ const newCycleFormValidationSchema = zod.object({
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
 
 export function Home() {
-  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
       task: "",
@@ -41,6 +41,7 @@ export function Home() {
 
   function handleCreateNewCycle(data: any) {
     console.log(data);
+    reset();
   }
 
   const task = watch("task");

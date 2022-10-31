7394bc72cb18b498cd1fabcb846b529440f355ae
MorenaNobre <morenagnobre@gmail.com>
2022-10-31 10:55:44 -0300

    Styles setup - GlobalStyle and defaultTheme

--- coffee-delivery-challenge/src/App.tsx
+++ coffee-delivery-challenge/src/App.tsx
@@ -1,7 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/theme/default";
+
 function App() {
   return (
-    <h1>Hello World</h1>      
-  )
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>Hello World</h1>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;

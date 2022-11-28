ab4292d26f83b5ade67d307e85d7d7b638fcae0d
MorenaNobre <morenagnobre@gmail.com>
2022-11-28 12:38:26 -0300

    Data | FormatMoney | Home front-end completed

--- coffee-delivery-challenge/src/pages/Home/components/CoffeeCard/index.tsx
+++ coffee-delivery-challenge/src/pages/Home/components/CoffeeCard/index.tsx
@@ -9,28 +9,43 @@ import {
   Tags,
 } from "./styles";
 import { ShoppingCart } from "phosphor-react";
+import { formatMoney } from "../../../../utils/formatMoney";
 
-export function CoffeeCard() {
+export interface Coffee {
+  id: number;
+  tags: string[];
+  name: string;
+  description: string;
+  photo: string;
+  price: number;
+}
+
+interface CoffeeProps {
+  coffee: Coffee;
+}
+
+export function CoffeeCard({ coffee }: CoffeeProps) {
+  const formattedPrice = formatMoney(coffee.price);
+
   return (
     <CoffeeCardContainer>
-      <img src="https://s3-alpha-sig.figma.com/img/55b1/f9ee/64600f98b2bae456b96fdc624c4b4f47?Expires=1669593600&Signature=EOGbXQYBeqYX1C7GcMygyM0XJaqbOU3ydZiPqL-NY6aHiskkr5FHMDc8QkVgOtGsiimXJJcmgjCy6XxHHUQcEHd1acupWJNluTrZXcnUpXvk0n6xEb5bBv6nQrioxaAGeaBA1L9Rd35dPGSi2ciwfqNKI7EctYFJ1qwJglnEqKW8bylgmjPXr6IEAXuZ2f4gRujfM~ikF64yMxA8i1TBrnpm4iPJqQ8dKZy3ncRYbjDnkyYWX-ngs51EbpbIkS5gsWuiafbgBmdCqEuso-t~lO0noqAiQ9lAS7mdfhvqrAsclTTn8gIXaDm--CciazC0p83m03Mf3YqLy-M9Sin8QA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" />
+      <img src={`/coffees/${coffee.photo}`} />
 
       <Tags>
-        <span>tradicional</span>
-        <span>com leite</span>
+        {coffee.tags.map((tag) => (
+          <span key={`${coffee.id}${tag}`}>{tag}</span>
+        ))}
       </Tags>
 
-      <Name>Expresso Tradicional</Name>
+      <Name>{coffee.name}</Name>
 
-      <Description>
-        O tradicional café feito com água quente e grãos moídos
-      </Description>
+      <Description>{coffee.description}</Description>
 
       <CardFooter>
         <div>
           <RegularText size="s">R$</RegularText>
           <TitleText size="m" color="text" as="strong">
-            9,90
+            {formattedPrice}
           </TitleText>
         </div>
         <AddCartWrapper>
--- coffee-delivery-challenge/src/pages/Home/components/OurCoffees/index.tsx
+++ coffee-delivery-challenge/src/pages/Home/components/OurCoffees/index.tsx
@@ -1,4 +1,5 @@
 import { TitleText } from "../../../../components/Typography";
+import { coffees } from "../../../../data/coffees";
 import { CoffeeCard } from "../CoffeeCard";
 import { CoffeeList, OurCoffeesContainer } from "./styles";
 
@@ -11,13 +12,9 @@ export function OurCoffees() {
         </TitleText>
 
         <CoffeeList>
-          <CoffeeCard />
-          <CoffeeCard />
-          <CoffeeCard />
-          <CoffeeCard />
-          <CoffeeCard />
-          <CoffeeCard />
-          <CoffeeCard />
+          {coffees.map((coffee) => (
+            <CoffeeCard key={coffee.id} coffee={coffee} />
+          ))}
         </CoffeeList>
       </OurCoffeesContainer>
     </>

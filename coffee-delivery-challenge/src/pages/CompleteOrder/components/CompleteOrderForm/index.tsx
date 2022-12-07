dde84eaad080e1e68af7b3755e7a22b1bfdd3623
MorenaNobre <morenagnobre@gmail.com>
2022-12-07 11:18:24 -0300

    PaymentMethodInput | PaymentMethodOptions components

--- coffee-delivery-challenge/src/pages/CompleteOrder/components/CompleteOrderForm/index.tsx
+++ coffee-delivery-challenge/src/pages/CompleteOrder/components/CompleteOrderForm/index.tsx
@@ -1,9 +1,10 @@
 import { TitleText } from "../../../../components/Typography";
 import { SectionTitle } from "../SectionTitle";
 import { CompleteOrderFormContainer, FormSectionContainer } from "./styles";
-import { MapPinLine } from "phosphor-react";
+import { MapPinLine, CurrencyDollar } from "phosphor-react";
 import { useTheme } from "styled-components";
 import { AddressForm } from "./AddressForm";
+import { PaymentMethodOptions } from "./PaymentMethodOptions";
 
 export function CompleteOrderForm() {
   const { colors } = useTheme();
@@ -21,6 +22,14 @@ export function CompleteOrderForm() {
           icon={<MapPinLine color={colors["brand-yellow-dark"]} size={22} />}
         />
         <AddressForm />
+      </FormSectionContainer>
+      <FormSectionContainer>
+        <SectionTitle
+          title="Pagamento"
+          subtitle="O pagamento é feito na entrega. Escolha a forma que deseja pagar"
+          icon={<CurrencyDollar color={colors["brand-purple"]} size={22} />}
+        />
+        <PaymentMethodOptions />
       </FormSectionContainer>
     </CompleteOrderFormContainer>
   );
--- coffee-delivery-challenge/src/pages/CompleteOrder/components/CompleteOrderForm/styles.ts
+++ coffee-delivery-challenge/src/pages/CompleteOrder/components/CompleteOrderForm/styles.ts
@@ -35,3 +35,9 @@ export const AddressFormContainer = styled.div`
     grid-column: span 2;
   }
 `
+
+export const PaymentMethodOptionsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 0.75rem;
+`

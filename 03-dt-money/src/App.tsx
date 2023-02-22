b0455213af492c1b00aab59e4d36a9745495c4ed
MorenaNobre <morenagnobre@gmail.com>
2023-02-22 16:04:50 -0300

    Setting GlobalStyle, DefaultTheme and types for styles. Styled-components will be used

--- 03-dt-money/src/App.tsx
+++ 03-dt-money/src/App.tsx
@@ -1,9 +1,11 @@
 import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <h1>Hello there</h1>
     </ThemeProvider>
   );

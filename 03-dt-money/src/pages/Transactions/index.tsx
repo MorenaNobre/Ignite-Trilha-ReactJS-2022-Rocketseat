20907e267902f0d59dcd8a250fa2a4ae0b946b13
MorenaNobre <morenagnobre@gmail.com>
2023-02-23 12:41:32 -0300

    Transaction table - structure and style

--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -1,11 +1,42 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+
+              <td>Venda</td>
+              <td>23/02/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+
+              <td>
+                <PriceHighlight variant="outcome">- R$ 59,60</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>14/02/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }

0ed914593ea4579ba01a922bdb6dbecf840b780e
MorenaNobre <morenagnobre@gmail.com>
2023-03-01 11:45:38 -0300

    Creating Transaction context

--- 03-dt-money/src/components/Summary/index.tsx
+++ 03-dt-money/src/components/Summary/index.tsx
@@ -1,7 +1,13 @@
+import { useContext } from "react";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { SummaryCard, SummaryContainer } from "./styles";
 import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from "phosphor-react";
 
 export function Summary() {
+  const { transactions } = useContext(TransactionsContext);
+
+  console.log(transactions);
+
   return (
     <SummaryContainer>
       <SummaryCard>
--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { SearchForm } from "./SearchForm";
 import { Summary } from "../../components/Summary";
@@ -7,6 +7,7 @@ import {
   TransactionsContainer,
   TransactionsTable,
 } from "./styles";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 interface Transaction {
   id: number;
@@ -18,18 +19,7 @@ interface Transaction {
 }
 
 export function Transactions() {
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-  async function loadTransactions() {
-    const response = await fetch("http://localhost:3000/transactions");
-    const data = await response.json();
-
-    setTransactions(data);
-  }
-
-  useEffect(() => {
-    loadTransactions();
-  }, []);
+  const { transactions } = useContext(TransactionsContext);
 
   return (
     <div>

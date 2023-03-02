f9ca12081289aaee7e295dedd45fd235b93d5001
MorenaNobre <morenagnobre@gmail.com>
2023-03-02 12:12:55 -0300

    Search for transactions

--- 03-dt-money/src/pages/Transactions/SearchForm/index.tsx
+++ 03-dt-money/src/pages/Transactions/SearchForm/index.tsx
@@ -4,6 +4,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 import { SearchFormContainer } from "./styles";
 import { MagnifyingGlass } from "phosphor-react";
+import { useContext } from "react";
+import { TransactionsContext } from "../../../contexts/TransactionsContext";
 
 const searchFormSchema = z.object({
   query: z.string(),
@@ -12,6 +14,8 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
+  const { fetchTransactions } = useContext(TransactionsContext);
+
   const {
     register,
     handleSubmit,
@@ -21,9 +25,7 @@ export function SearchForm() {
   });
 
   async function handleSearchTransactions(data: SearchFormInputs) {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-
-    console.log(data);
+    await fetchTransactions(data.query);
   }
 
   return (
